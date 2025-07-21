3e6549b6aa74d9efaec93a8fed8177b78992a7bf
md-mejbaul-haque <mejba.s@gmail.com>
2025-07-21 12:58:06 +0600

    Buttons added

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tkinter import  *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
@@ -16,21 +16,25 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Label
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="password")
+password_label = Label(text="password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
-password_entry = Entry(width=21)
+website_entry = Entry(width=55)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=55)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=36)
 password_entry.grid(row=3, column=1)
 
-
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=46)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
